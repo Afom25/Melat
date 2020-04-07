d12cfb35fb2f7a3685ba87dfd15d41d1c2887502
hidri <agthidri2400@gmail.com>
2020-04-07 23:14:24 +0300

    This commit will add the change name

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+
+$ (function(){
+    new WOW().init();   
+});
+$(window).on('load',function(){
+    $("#").addClass("animated fadeInDown");
+    $("#form-pay").addClass("animated fadeInRight");
+    $("#").addClass("animated zoomIn");
+    $("#").addClass("animated fadeDown infinite");
+
+});
